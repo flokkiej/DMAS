5d9c99466b4b863b6a4751f3050834289f443b7f
folkert <flokkiej@gmail.com>
2015-09-15 11:45:04 +0200

    Objectified the Grid

--- hello.py
+++ hello.py
@@ -1,34 +1,46 @@
 import sys
 
-class Agent(object):
-	"""docstring for Agent"""
+class agent(object):
+	"""docstring for agent"""
 	def __init__(self, char):
-		super(Agent, self).__init__()
+		super(agent, self).__init__()
 		self.char = char
 	def out(self):
 		sys.stdout.write(self.char)
 		sys.stdout.write(' ')
 		
 
+class grid(object):
+	"""docstring for grid"""
+	def __init__(self, gridSize):
+		super(grid, self).__init__()
+		self.size = gridSize
+		self.initGrid()
+	def returnSize(self):
+		return self.size
+	def initGrid(self):
+		self.grid = [[0 for x in xrange(self.size)] for x in xrange(self.size)] 
+	def fillGrid(self):
+		for i in xrange(self.size):
+			for j in xrange(self.size):
+				self.grid[i][j] = agent('a')
+	def printGrid(self):
+		for i in xrange(self.size):
+			for j in xrange(self.size):
+				self.grid[i][j].out()
+			print "\n"
+
 def main():
 
-	gridSize = 5
-
-	grid = [[0 for x in xrange(gridSize)] for x in xrange(gridSize)] 
-
-	for i in xrange(gridSize):
-		for j in xrange(gridSize):
-			grid[i][j] = Agent('a')
-
-	printGrid(grid, gridSize)
+	gridSize = 10
+	g = grid(gridSize)
+	g.fillGrid()
+	g.printGrid()
+#	printGrid(grid, gridSize)
 	return 
 
 def printGrid(grid, gridSize):
 
-	for i in xrange(gridSize):
-		for j in xrange(gridSize):
-			grid[i][j].out()
-		print "\n"
 	return
 
 
